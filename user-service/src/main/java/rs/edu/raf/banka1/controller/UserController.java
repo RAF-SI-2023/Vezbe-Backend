eb6dc3271b7e1d740a444f87b88383cb0ea2da9c
Marko Mudrinić <mudrinic.mare@gmail.com>
2024-03-09 11:25:21 +0100

    Reseni komentari

--- user-service/src/main/java/rs/edu/raf/banka1/controller/UserController.java
+++ user-service/src/main/java/rs/edu/raf/banka1/controller/UserController.java
@@ -18,7 +18,7 @@ public class UserController {
     @Operation(summary = "Pozdrav od servisa", description = "Vraca poruku pozdrava od user-service-a")
     public ResponseEntity<String> helloFromService() {
         // Vraca odgovor "Hello from user-service!"
-        System.out.println("Hello from user-service!");
+        System.out.println("Zdravo iz korisnickog servisa!");
         return ResponseEntity.ok("Hello from user-service!");
     }
 
